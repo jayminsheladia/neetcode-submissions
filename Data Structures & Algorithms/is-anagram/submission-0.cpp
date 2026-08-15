51223ee0be4510b5ab26c217fb720b6502e374d3
jayminsheladia <111639420+jayminsheladia@users.noreply.github.com>
2026-08-15 08:53:48 -0700

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int size1, size2;
+        size1 = s.size();
+        size2 = t.size();
+        if(size1 != size2)
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i = 0; i < size1; i++){
+            if(s[i] != t[i])
+                return false;
+        }
+        return true;
+    }
+};
